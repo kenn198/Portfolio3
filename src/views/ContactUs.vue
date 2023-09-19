b2568370c11c130690090caceafa8b37e0011340
Ken Tay <taykenneth@rocketmail.com>
2023-09-19 14:23:25 +1000

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   cypress.config.js 	new file:   cypress/e2e/example.cy.js 	new file:   cypress/e2e/jsconfig.json 	new file:   cypress/fixtures/example.json 	new file:   cypress/support/commands.js 	new file:   cypress/support/e2e.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/images/diego-ph-fIq0tET6llw-unsplash.jpg 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/components/FooterComponent.vue 	new file:   src/components/HelloWorld.vue 	new file:   src/components/TheWelcome.vue 	new file:   src/components/WelcomeItem.vue 	new file:   src/components/__tests__/HelloWorld.spec.js 	new file:   src/components/icons/IconCommunity.vue 	new file:   src/components/icons/IconDocumentation.vue 	new file:   src/components/icons/IconEcosystem.vue 	new file:   src/components/icons/IconSupport.vue 	new file:   src/components/icons/IconTooling.vue 	new file:   src/main.js 	new file:   src/router/index.js 	new file:   src/views/AboutView.vue 	new file:   src/views/ContactUs.vue 	new file:   src/views/HomeView.vue 	new file:   vite.config.js 	new file:   vitest.config.js

---
+++ src/views/ContactUs.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="about-us-form">
+      <h2>About Us Feedback</h2>
+      <form @submit.prevent="handleSubmit">
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="formData.name" required />
+        </div>
+  
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="formData.email" required />
+        </div>
+  
+        <div class="form-group">
+          <label for="feedback">Feedback:</label>
+          <textarea id="feedback" v-model="formData.feedback" required></textarea>
+        </div>
+  
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const formData = ref({
+    name: '',
+    email: '',
+    feedback: ''
+  });
+  
+  const handleSubmit = () => {
+    // Handle the form submission logic here
+    // For demonstration purposes, we'll just log the form data
+    console.log(formData.value);
+  
+    // Reset the form
+    formData.value = {
+      name: '',
+      email: '',
+      feedback: ''
+    };
+  };
+  </script>
+  
+  <style scoped>
+  .about-us-form {
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  
+  .form-group {
+    margin-bottom: 20px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 10px;
+  }
+  
+  input[type="text"],
+  input[type="email"],
+  textarea {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  
+  button {
+    padding: 10px 15px;
+    background-color: #007BFF;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background-color: #0056b3;
+  }
+  </style>
+  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,106 @@
+<template>
+  <section class="portfolio-home">
+    <header class="portfolio-header">
+      <h1>John Doe</h1>
+      <p>Web Developer & Designer</p>
+    </header>
+
+    <section class="portfolio-intro">
+      <p>
+        Hello! I'm John, a passionate web developer with over 5 years of experience. I love creating responsive and user-friendly websites. Check out my projects below!
+      </p>
+    </section>
+
+    <section class="portfolio-projects">
+      <h2>My Projects</h2>
+      <div class="projects-grid">
+        <div class="project-card" v-for="project in projects" :key="project.id">
+          <img :src="project.image" :alt="project.name" />
+          <h3>{{ project.name }}</h3>
+          <p>{{ project.description }}</p>
+        </div>
+      </div>
+    </section>
+
+    <footer class="portfolio-footer">
+      <p>Thank you for visiting! Let's <a href="mailto:john@example.com">connect</a>.</p>
+    </footer>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+import projectImage1 from '../assets/images/diego-ph-fIq0tET6llw-unsplash.jpg';
+
+
+const projects = ref([
+  {
+    id: 1,
+    name: 'Project One',
+    description: 'A description of project one.',
+    image: projectImage1
+  },
+  {
+    id: 2,
+    name: 'Project Two',
+    description: 'A description of project two.',
+    image: projectImage1
+  },
+  // ... add more projects as needed
+]);
+</script>
+
+<style scoped>
+.portfolio-home {
+  font-family: 'Arial', sans-serif;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.portfolio-header {
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.portfolio-header h1 {
+  font-size: 2.5em;
+  margin-bottom: 10px;
+}
+
+.portfolio-intro {
+  text-align: center;
+  margin-bottom: 50px;
+}
+
+.portfolio-projects {
+  margin-bottom: 50px;
+}
+
+.projects-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  gap: 20px;
+}
+
+.project-card {
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.project-card img {
+  width: 100%;
+  height: auto;
+}
+
+.project-card h3 {
+  text-align: center;
+  padding: 10px 0;
+}
+
+.portfolio-footer {
+  text-align: center;
+}
+</style>
